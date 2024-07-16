0ee82d8512456387110d299b787d02f69022c95f
henNet <dmf.n123@gmail.com>
2024-07-16 03:58:14 -0300

    feat: Jogo "Se Liga Rambo" iniciado - Colocado o título do jogo (melhorar) - Adicionado o personagem com movimentação up e down

---
+++ src/scenes/Game.js
@@ -0,0 +1,83 @@
+import { Scene } from "phaser";
+
+export class Game extends Scene {
+  /* Variavel que guarda a posição atual do player
+  position: 0; means in the top
+  position: 1; means in the middle
+  position: 1; means in the bottom */
+  playerPosition = {
+    position: 1,
+    coords: [
+      { x: 150, y: window.innerHeight * 0.2 },
+      { x: 150, y: window.innerHeight / 2 },
+      { x: 150, y: window.innerHeight * 0.8 },
+    ],
+  };
+
+  constructor() {
+    super("Game");
+  }
+
+  preload() {
+    this.load.setPath("assets");
+
+    // this.load.image("background", "cenario.png");
+    this.load.image("logo", "title.png");
+    this.load.spritesheet("player", "player.png", {
+      frameWidth: 150,
+      frameHeight: 144,
+    });
+  }
+
+  create() {
+    // this.add.image(window.innerWidth / 2, window.innerHeight / 2, "background");
+    this.add
+      .image(window.innerWidth / 2, window.innerHeight / 2, "logo")
+      .setDepth(100);
+
+    this.player = this.physics.add.sprite(
+      150,
+      window.innerHeight / 2,
+      "player"
+    );
+
+    this.anims.create({
+      key: "run",
+      frames: this.anims.generateFrameNumbers("player", { start: 0, end: 1 }),
+      frameRate: 10,
+      repeat: -1 /* -1 means loop */,
+    });
+
+    this.player.anims.play("run");
+
+    this.arrowUp = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.UP
+    );
+
+    this.arrowDown = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.DOWN
+    );
+  }
+
+  update() {
+    if (Phaser.Input.Keyboard.JustDown(this.arrowUp)) {
+      if (this.playerPosition.position !== 0) {
+        let pos = (this.playerPosition.position -= 1);
+        this.player.setPosition(
+          this.playerPosition.coords[pos].x,
+          this.playerPosition.coords[pos].y
+        );
+      }
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(this.arrowDown)) {
+      if (this.playerPosition.position !== 2) {
+        let pos = (this.playerPosition.position += 1);
+        this.player.setPosition(
+          this.playerPosition.coords[pos].x,
+          this.playerPosition.coords[pos].y
+        );
+      }
+    }
+  }
+}
